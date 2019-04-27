ad209f01490b13aa77515a4231d5898914a5a1fa
djdt <10266332+djdt@users.noreply.github.com>
2019-04-27 11:56:05 +1000

    fix extent

--- data.py
+++ data.py
@@ -12,7 +12,7 @@ class LaserData(object):
         )
 
     def get(self, config: LaserConfig, **kwargs) -> np.ndarray:
-        data = self.data.copy()
+        data = self.data
 
         if "extent" in kwargs:
             x0, x1, y0, y1 = kwargs.get("extent", (0.0, 0.0, 0.0, 0.0))
@@ -20,7 +20,8 @@ class LaserData(object):
             x0, x1 = int(x0 / px), int(x1 / px)
             y0, y1 = int(y0 / py), int(y1 / py)
             # We have to invert the extent, as mpl use bottom left y coords
-            data = data[-y1:-y0, x0:x1]
+            ymax = data.shape[0]
+            data = data[ymax - y1:ymax - y0, x0:x1]
 
         if kwargs.get("calibrate", False):
             data = self.calibration.calibrate(data)
