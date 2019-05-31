ef8d964c48a72ec45b748c626d928ab68faeab8f
djdt <10266332+djdt@users.noreply.github.com>
2019-05-31 16:58:14 +1000

    possible storage of cal points

---
+++ calc.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+from typing import Tuple
+
+
+def get_weights(x: np.ndarray, weighting: str) -> np.ndarray:
+    if weighting == "x":
+        return x
+    if weighting == "1/x":
+        return 1.0 / x
+    elif weighting == "1/(x^2)":
+        return 1.0 / (x ** 2.0)
+    else:  # Default is no weighting
+        return None
+
+
+def weighted_rsq(x: np.ndarray, y: np.ndarray, w: np.ndarray = None) -> float:
+    c = np.cov(x, y, aweights=w)
+    d = np.diag(c)
+    stddev = np.sqrt(d.real)
+    c /= stddev[:, None]
+    c /= stddev[None, :]
+
+    np.clip(c.real, -1, 1, out=c.real)
+    if np.iscomplexobj(c):
+        np.clip(c.imag, -1, 1, out=c.imag)
+
+    return c[0, 1] ** 2.0
+
+
+def weighted_linreg(
+    x: np.ndarray, y: np.ndarray, w: np.ndarray = None
+) -> Tuple[float, float, float]:
+    m, b = np.polyfit(x, y, 1, w=w)
+    r2 = weighted_rsq(x, y, w)
+    return m, b, r2
--- calibration.py
+++ calibration.py
@@ -1,13 +1,34 @@
 import numpy as np
 
+from .calc import get_weights, weighted_linreg
+
 
 class LaserCalibration(object):
     DEFAULT_UNIT = ""
 
-    def __init__(self, intercept: float = 0.0, gradient: float = 1.0, unit: str = None):
-        self.gradient = gradient
+    def __init__(
+        self,
+        intercept: float = 0.0,
+        gradient: float = 1.0,
+        rsq: float = None,
+        points: np.ndarray = None,
+        weighting: str = None,
+        unit: str = None,
+    ):
         self.intercept = intercept
+        self.gradient = gradient
+        self.rsq = rsq
+
+        self.points = points
+        self.weighting = weighting
         self.unit = unit if unit is not None else LaserCalibration.DEFAULT_UNIT
+
+    def update_from_points(self) -> None:
+        x = self.points[:, 0]
+        y = self.points[:, 1]
+        self.intercept, self.gradient, self.rsq = weighted_linreg(
+            x, y, get_weights(x, self.weighting)
+        )
 
     def calibrate(self, data: np.ndarray) -> np.ndarray:
         if self.intercept == 0.0 and self.gradient == 1.0:
