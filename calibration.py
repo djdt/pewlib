65dab9fad62f76be363746e3f02a7e7ad63d1b58
djdt <10266332+djdt@users.noreply.github.com>
2019-06-01 13:23:39 +1000

    improve str

--- calibration.py
+++ calibration.py
@@ -4,27 +4,28 @@ from .calc import get_weights, weighted_linreg
 
 
 class LaserCalibration(object):
-    DEFAULT_UNIT = ""
-
     def __init__(
         self,
         intercept: float = 0.0,
         gradient: float = 1.0,
-        unit: str = None,
         rsq: float = None,
         points: np.ndarray = None,
         weighting: str = None,
+        unit: str = "",
     ):
         self.intercept = intercept
         self.gradient = gradient
-        self.unit = unit if unit is not None else LaserCalibration.DEFAULT_UNIT
+        self.unit = unit
 
         self.rsq = rsq
         self.points = points
         self.weighting = weighting
 
     def __str__(self) -> str:
-        return f"{self.gradient:.4g}*x{self.intercept:+.4g}"
+        s = f"y = {self.gradient:.4g} · x - {self.intercept:.4g}"
+        if self.rsq is not None:
+            s += "\nr² = {self.rsq:.4f}"
+        return s
 
     def concentrations(self) -> np.ndarray:
         if self.points is None:
@@ -36,9 +37,8 @@ class LaserCalibration(object):
             return np.array([], dtype=float)
         return self.points[:, 1]
 
-    def update_from_points(self) -> None:
-        x = self.points[:, 0]
-        y = self.points[:, 1]
+    def update_linreg(self) -> None:
+        x, y = self.points[:, 0], self.points[:, 1]
         self.intercept, self.gradient, self.rsq = weighted_linreg(
             x, y, get_weights(x, self.weighting)
         )
@@ -47,3 +47,18 @@ class LaserCalibration(object):
         if self.intercept == 0.0 and self.gradient == 1.0:
             return data
         return (data - self.intercept) / self.gradient
+
+    @classmethod
+    def from_points(
+        cls, points: np.ndarray, weighting: str = None, unit: str = ""
+    ):  # type: ignore
+        x, y = points[:, 0], points[:, 1]
+        m, b, rsq = weighted_linreg(x, y, get_weights(x, weighting))
+        return cls(
+            intercept=b,
+            gradient=m,
+            rsq=rsq,
+            points=points,
+            weighting=weighting,
+            unit=unit,
+        )
