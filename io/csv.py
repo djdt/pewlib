f813862295176a6e66f6cbd48b65216649a65497
djdt <10266332+djdt@users.noreply.github.com>
2019-06-06 18:13:26 +1000

    return as structured

--- io/csv.py
+++ io/csv.py
@@ -3,17 +3,18 @@ import numpy as np
 from .error import LaserLibException
 
 
-def load(path: str) -> np.ndarray:
+def load(path: str, isotope: str = "CSV") -> np.ndarray:
     with open(path, "rb") as fp:
         cleaned = (line.replace(b";", b",").replace(b"\t", b",") for line in fp)
         try:
             data = np.genfromtxt(cleaned, delimiter=b",", comments=b"#", dtype=float)
         except ValueError as e:
             raise LaserLibException("Could not parse file.") from e
-        if data.ndim != 2:
-            raise LaserLibException(f"Invalid data dimensions '{data.ndim}'.")
+    if data.ndim != 2:
+        raise LaserLibException(f"Invalid data dimensions '{data.ndim}'.")
 
-        return data
+    data.dtype = [(isotope, data.dtype)]
+    return data
 
 
 def save(path: str, data: np.ndarray, header: str = "") -> None:
