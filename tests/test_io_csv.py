f56d52a02fe99b6175a22f7b33bdc7e897908d0d
djdt <10266332+djdt@users.noreply.github.com>
2019-06-07 10:14:39 +1000

    improved tests for csv and thermo, they should not be able to load each others data

--- tests/test_io_csv.py
+++ tests/test_io_csv.py
@@ -7,85 +7,25 @@ from laserlib import io
 from laserlib.io import csv
 
 data_path = os.path.join(os.path.dirname(__file__), "data", "csv")
+data_path_thermo = os.path.join(os.path.dirname(__file__), "data", "thermo")
 
 
 class CSVIOTest(unittest.TestCase):
-    def test_load_raw(self):
-        data = csv.load_raw(os.path.join(data_path, "csv.csv"))
-
-        self.assertEqual(data.shape, (3, 3))
-        self.assertEqual(np.sum(data), 12.0)
-
-        data = csv.load_raw(os.path.join(data_path, "csv_header.csv"))
-
-        self.assertEqual(data.shape, (3, 3))
-        self.assertEqual(np.sum(data), 12.0)
+    def test_fail_thermo(self):
+        with self.assertRaises(io.error.LaserLibException):
+            csv.load(os.path.join(data_path_thermo, "icap.csv"))
 
     def test_load(self):
-        laser = csv.load(os.path.join(data_path, "csv.csv"))
+        data = csv.load(os.path.join(data_path, "csv.csv"))
 
-        self.assertIn("_", laser.data)
-        self.assertEqual(laser.data["_"].data.shape, (3, 3))
-        self.assertEqual(np.sum(laser.data["_"].data), 12.0)
-        self.assertEqual(laser.data["_"].calibration.intercept, 0.0)
-        self.assertEqual(laser.data["_"].calibration.gradient, 1.0)
-        self.assertEqual(laser.data["_"].calibration.unit, "")
-
-    def test_load_header(self):
-        laser = csv.load(os.path.join(data_path, "csv_header.csv"), read_header=True)
-
-        self.assertIn("_", laser.data)
-        self.assertEqual(laser.data["_"].data.shape, (3, 3))
-        self.assertEqual(np.sum(laser.data["_"].data), 12.0)
-
-        self.assertEqual(laser.data["_"].calibration.intercept, 2.0)
-        self.assertEqual(laser.data["_"].calibration.gradient, 3.0)
-        self.assertEqual(laser.data["_"].calibration.unit, "u")
-
-        self.assertEqual(laser.config.spotsize, 0.1)
-        self.assertEqual(laser.config.speed, 0.2)
-        self.assertEqual(laser.config.scantime, 0.3)
-
-    def test_load_missing_header(self):
-        with self.assertRaises(io.error.LaserLibException):
-            csv.load(os.path.join(data_path, "csv.csv"), read_header=True)
+        self.assertEqual(data.dtype.names, ("CSV",))
+        self.assertEqual(data.shape, (3, 3))
+        self.assertEqual(np.sum(data["CSV"]), 12.0)
 
     def test_save(self):
-        data = csv.load_raw(os.path.join(data_path, "csv.csv"))
+        data = csv.load(os.path.join(data_path, "csv.csv"))
         temp = tempfile.NamedTemporaryFile()
-        csv.save(temp.name, data, header="")
-        self.assertTrue(np.all(data == csv.load_raw(temp.name)))
+        csv.save(temp.name, data["CSV"])
+        self.assertTrue(np.all(data["CSV"] == csv.load(temp.name)["CSV"]))
 
         temp.close()
-
-    def test_save_header(self):
-        laser = csv.load(os.path.join(data_path, "csv_header.csv"), read_header=True)
-        temp = tempfile.NamedTemporaryFile()
-        csv.save(
-            temp.name, laser.data["_"].data, header=csv.make_header(laser, "_")
-        )
-
-        laser2 = csv.load(temp.name, read_header=True)
-
-        temp.close()
-
-        self.assertEqual(laser.config.spotsize, laser2.config.spotsize)
-        self.assertEqual(laser.config.speed, laser2.config.speed)
-        self.assertEqual(laser.config.scantime, laser2.config.scantime)
-
-        self.assertIn("_", laser2.data)
-
-        self.assertTrue(np.all(laser.data["_"].data == laser2.data["_"].data))
-
-        self.assertEqual(
-            laser.data["_"].calibration.intercept,
-            laser2.data["_"].calibration.intercept,
-        )
-        self.assertEqual(
-            laser.data["_"].calibration.gradient,
-            laser2.data["_"].calibration.gradient,
-        )
-        self.assertEqual(
-            laser.data["_"].calibration.unit,
-            laser2.data["_"].calibration.unit,
-        )
--- tests/test_io_thermo.py
+++ tests/test_io_thermo.py
@@ -2,12 +2,18 @@ import unittest
 import numpy as np
 import os.path
 
+from laserlib import io
 from laserlib.io import thermo
 
+data_path_csv = os.path.join(os.path.dirname(__file__), "data", "csv")
 data_path = os.path.join(os.path.dirname(__file__), "data", "thermo")
 
 
 class ThermoIOTest(unittest.TestCase):
+    def test_fail_csv(self):
+        with self.assertRaises(io.error.LaserLibException):
+            thermo.load(os.path.join(data_path_csv, "csv.csv"))
+
     def test_load_icap(self):
         data = thermo.load(os.path.join(data_path, "icap.csv"))
 
