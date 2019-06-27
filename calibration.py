ac9dcea8ec10195de01610e7f3fd30f10d84e1f5
djdt <10266332+djdt@users.noreply.github.com>
2019-06-27 11:55:16 +1000

    linreg wont crash when points not set

--- calibration.py
+++ calibration.py
@@ -9,7 +9,7 @@ class LaserCalibration(object):
         intercept: float = 0.0,
         gradient: float = 1.0,
         rsq: float = None,
-        points: np.ndarray = [[], []],
+        points: np.ndarray = [],
         weighting: str = None,
         unit: str = "",
     ):
@@ -18,7 +18,7 @@ class LaserCalibration(object):
         self.unit = unit
 
         self.rsq = rsq
-        self.points = np.array(points, dtype=float)
+        self.points = np.array(points, dtype=np.float64)
         self.weighting = weighting
 
     def __str__(self) -> str:
@@ -39,10 +39,13 @@ class LaserCalibration(object):
 
     def update_linreg(self) -> None:
         no_nans = self.points[~np.isnan(self.points).any(axis=1)]
-        x, y = no_nans[:, 0], no_nans[:, 1]
-        self.intercept, self.gradient, self.rsq = weighted_linreg(
-            x, y, get_weights(x, self.weighting)
-        )
+        if no_nans.size == 0:
+            self.intercept, self.gradient, self.rsq = 0.0, 1.0, None
+        else:
+            x, y = no_nans[:, 0], no_nans[:, 1]
+            self.intercept, self.gradient, self.rsq = weighted_linreg(
+                x, y, get_weights(x, self.weighting)
+            )
 
     def calibrate(self, data: np.ndarray) -> np.ndarray:
         if self.intercept == 0.0 and self.gradient == 1.0:
