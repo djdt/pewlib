3a7379518b80dd193041207a41808714e3a4c79d
djdt <10266332+djdt@users.noreply.github.com>
2019-06-27 15:29:46 +1000

    back to empty array for points

--- calibration.py
+++ calibration.py
@@ -9,7 +9,7 @@ class LaserCalibration(object):
         intercept: float = 0.0,
         gradient: float = 1.0,
         rsq: float = None,
-        points: np.ndarray = None,
+        points: np.ndarray = [],
         weighting: str = None,
         unit: str = "",
     ):
@@ -18,8 +18,6 @@ class LaserCalibration(object):
         self.unit = unit
 
         self.rsq = rsq
-        if points is None:
-            points = np.full((2, 1), np.nan, dtype=np.float64)
         self.points = np.array(points, dtype=np.float64)
         self.weighting = weighting
 
@@ -30,20 +28,27 @@ class LaserCalibration(object):
         return s
 
     def concentrations(self) -> np.ndarray:
+        if self.points.size == 0:
+            return np.array([], dtype=np.float64)
         return self.points[:, 0]
 
     def counts(self) -> np.ndarray:
+        if self.points.size == 0:
+            return np.array([], dtype=np.float64)
         return self.points[:, 1]
 
     def update_linreg(self) -> None:
-        no_nans = self.points[~np.isnan(self.points).any(axis=1)]
-        if no_nans.size == 0:
+        if self.points.size == 0:
             self.gradient, self.intercept, self.rsq = 1.0, 0.0, None
         else:
-            x, y = no_nans[:, 0], no_nans[:, 1]
-            self.gradient, self.intercept, self.rsq = weighted_linreg(
-                x, y, get_weights(x, self.weighting)
-            )
+            no_nans = self.points[~np.isnan(self.points).any(axis=1)]
+            if no_nans.size == 0:
+                self.gradient, self.intercept, self.rsq = 1.0, 0.0, None
+            else:
+                x, y = no_nans[:, 0], no_nans[:, 1]
+                self.gradient, self.intercept, self.rsq = weighted_linreg(
+                    x, y, get_weights(x, self.weighting)
+                )
 
     def calibrate(self, data: np.ndarray) -> np.ndarray:
         if self.intercept == 0.0 and self.gradient == 1.0:
