fix broken weighting

calc.py
@@ -35,6 +35,6 @@ def weighted_rsq(x: np.ndarray, y: np.ndarray, w: np.ndarray = None) -> float:
 def weighted_linreg(
     x: np.ndarray, y: np.ndarray, w: np.ndarray = None
 ) -> Tuple[float, float, float]:
-    m, b = np.polyfit(x, y, 1, w=w)
+    m, b = np.polyfit(x, y, 1, w=w if w is None else np.sqrt(w))
     r2 = weighted_rsq(x, y, w)
     return m, b, r2