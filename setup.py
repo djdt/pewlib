5ab8ec2a701012ad128836338ec0c24cb04965c1
djdt <10266332+djdt@users.noreply.github.com>
2019-10-03 21:47:42 +1000

    more detatiled setup

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
 from setuptools import setup, find_packages
 
-setup(name="pew", packages=find_packages())
+from pew import __version__
+
+setup(
+    name="pew",
+    description="Import / export library for LA-ICP-MS data.",
+    author="djdt",
+    version=__version__,
+    packages=find_packages(include=["pew", "pew.*"]),
+    install_requires=["numpy"],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+)
