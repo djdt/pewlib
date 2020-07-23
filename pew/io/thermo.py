88bff12797b7d8509139f7a2890b8b2b54bde5da
djdt <10266332+djdt@users.noreply.github.com>
2020-07-23 13:27:40 +1000

    read params from files, correctly determine col or row

--- pew/io/thermo.py
+++ pew/io/thermo.py
@@ -5,21 +5,14 @@ from pew.io.error import PewException
 from typing import Generator, List, Set, Tuple, TextIO
 
 
-def icap_csv_columns_read_data(
-    path: str,
-    delimiter: str = None,
-    comma_decimal: bool = False,
-    use_analog: bool = False,
+def _icap_csv_columns_read(
+    path: str, line_type: str, delimiter: str = None, comma_decimal: bool = False
 ) -> np.ndarray:
-    # Greatly speeds up reading
     def _read_lines(
-        fp: TextIO, analog: bool = False, replace_decimal: bool = False
+        fp: TextIO, line_type: str, replace_decimal: bool = False
     ) -> Generator[str, None, None]:
         for line in fp:
-            if (
-                line.startswith("MainRuns")
-                and ("Analog" if analog else "Counter") in line
-            ):
+            if line.startswith("MainRuns") and line_type in line:
                 yield line.replace(",", ".") if replace_decimal else line
 
     with open(path, "r", encoding="utf-8-sig") as fp:
@@ -40,26 +33,50 @@ def icap_csv_columns_read_data(
             ("data", np.float64, nlines),
         ]
         try:
-            data = np.genfromtxt(
-                _read_lines(fp, use_analog, replace_decimal=comma_decimal),
+            return np.genfromtxt(
+                _read_lines(fp, line_type, replace_decimal=comma_decimal),
                 dtype=dtype,
                 delimiter=delimiter,
             )
         except ValueError as e:
-            raise PewException("Could not parse iCap CSV as columns.") from e
+            raise PewException("Could not read iCap CSV (samples in columns).") from e
 
-        nscans = np.amax(data["scan"]) + 1
 
-        names, idx = np.unique(data["isotope"], return_index=True)
-        names = names[np.argsort(idx)]
+def icap_csv_columns_read_data(
+    path: str,
+    delimiter: str = None,
+    comma_decimal: bool = False,
+    use_analog: bool = False,
+) -> np.ndarray:
+    line_type = "Analog" if use_analog else "Counter"
+    data = _icap_csv_columns_read(
+        path, line_type=line_type, delimiter=delimiter, comma_decimal=comma_decimal
+    )
+    nscans = np.amax(data["scan"]) + 1
+
+    names, idx = np.unique(data["isotope"], return_index=True)
+    names = names[np.argsort(idx)]
 
     structured = np.empty(
-        (nlines, nscans), dtype=[(name.decode(), np.float64) for name in names],
+        (data["data"].shape[1], nscans),
+        dtype=[(name.decode(), np.float64) for name in names],
     )
     for name in names:
         structured[name.decode()] = data[data["isotope"] == name]["data"].T
 
     return structured
+
+
+def icap_csv_columns_read_params(
+    path: str, delimiter: str = None, comma_decimal: bool = False,
+) -> dict:
+    data = _icap_csv_columns_read(
+        path, line_type="Time", delimiter=delimiter, comma_decimal=comma_decimal
+    )
+    data = data[data["isotope"] == data["isotope"][0]]
+    scantime = np.round(np.nanmean(np.diff(data, axis=0)), 4)
+
+    return dict(scantime=scantime)
 
 
 def icap_csv_rows_read_data(
@@ -102,7 +119,7 @@ def icap_csv_rows_read_data(
                 usecols=cols,
             )
         except ValueError as e:
-            raise PewException("Could not parse iCap CSV as rows.") from e
+            raise PewException("Could not read iCap CSV (samples in rows).") from e
 
     structured = np.empty(
         (data.shape[0], nscans), dtype=[(name.decode(), np.float64) for name in names],
@@ -113,32 +130,48 @@ def icap_csv_rows_read_data(
     return structured
 
 
-# def preprocess_file(path: str) -> Tuple[List[str], float, Tuple[int, int]]:
-#     names: Set[str] = set()
-#     time = 0.0
-#     nscans = 0
+def icap_csv_rows_read_params(
+    path: str, delimiter: str = None, comma_decimal: bool = False,
+) -> dict:
+    def _read_lines(
+        fp: TextIO, replace_decimal: bool = False
+    ) -> Generator[str, None, None]:
+        for line in fp:
+            yield line.replace(",", ".") if replace_decimal else line
 
-#     lines = clean_lines(path)
-#     line1 = next(lines)
-#     if "Sample" not in line1:
-#         raise PewException("Unknown iCap CSV formatting.")
-#     nlines = line1.count(";") - 4
+    with open(path, "r", encoding="utf-8-sig") as fp:
+        line = fp.readline()
+        if delimiter is None:
+            delimiter = line[0]  # Should be delimiter
 
-#     for line in lines:
-#         run_type, n, name, data_type, data = line.split(";", 4)
-#         if name:
-#             names.add(name)
-#         nscans = max(nscans, int(n or -1) + 1)
-#         if run_type != "MainRuns":
-#             continue
-#         if data_type == "Time":
-#             time = max(time, float(next(s for s in data.split(";") if s)))
+        run_mask = np.genfromtxt([line], dtype="S8", delimiter=delimiter) == b"MainRuns"
+        if np.count_nonzero(run_mask) == 0:
+            raise PewException("Invalid iCap export, expected samples in rows.")
 
-#     return (
-#         sorted(names, key=lambda f: int("".join(filter(str.isdigit, f)))),
-#         np.round(time / nscans, 4),
-#         (nlines, nscans),
-#     )
+        fp.readline()  # scans
+        isotopes = np.genfromtxt([fp.readline()], dtype="S12", delimiter=delimiter)
+        dtype = np.genfromtxt([fp.readline()], dtype="S4", delimiter=delimiter)
+
+        cols = np.nonzero(
+            np.logical_and.reduce((isotopes == isotopes[2], dtype == b"Time", run_mask))
+        )[0]
+        names, idx = np.unique(isotopes[cols], return_index=True)
+        names = names[np.argsort(idx)]
+
+        try:
+            data = np.genfromtxt(
+                _read_lines(fp, replace_decimal=comma_decimal),
+                dtype=np.float64,
+                delimiter=delimiter,
+                usecols=cols,
+                max_rows=1,
+            )
+        except ValueError as e:
+            raise PewException("Could not read iCap CSV (samples in rows).") from e
+
+    scantime = np.round(np.nanmean(np.diff(data, axis=0)), 4)
+
+    return dict(scantime=scantime)
 
 
 def load(path: str, samples_in_rows: bool = None, full: bool = False) -> np.ndarray:
@@ -158,7 +191,8 @@ def load(path: str, samples_in_rows: bool = None, full: bool = False) -> np.ndar
 
     """
     if samples_in_rows is None:
-        lines = [next(open(path, "r")) for i in range(3)]
+        with open(path, "r") as fp:
+            lines = [next(fp) for i in range(3)]
         if "MainRuns" in lines[0]:
             samples_in_rows = True
         elif "MainRuns" in lines[2]:
@@ -167,12 +201,15 @@ def load(path: str, samples_in_rows: bool = None, full: bool = False) -> np.ndar
             raise PewException("Unknown iCap CSV format.")
     if samples_in_rows:
         data = icap_csv_rows_read_data(path)
+        if full:
+            params = icap_csv_rows_read_params(path)
     else:
         data = icap_csv_columns_read_data(path)
+        if full:
+            params = icap_csv_columns_read_params(path)
 
     if full:
-        return data, dict(scantime=0.250)
-        # return data, dict(scantime=scan_time)
+        return data, dict(scantime=params["scantime"])
     else:
         return data
 
@@ -182,12 +219,15 @@ if __name__ == "__main__":
 
     t0 = time.time()
     d = icap_csv_rows_read_data(
-        "/home/tom/Downloads/thermo/20200721_GelatineStdsLaserJacob.csv",
+        "/home/tom/Desktop/20200721_GelatineStdsLaserJacob.csv",
     )
+    p = icap_csv_rows_read_params(
+        "/home/tom/Desktop/20200721_GelatineStdsLaserJacob.csv"
+    )
+    print(p)
     t1 = time.time()
     d = icap_csv_columns_read_data(
-        "/home/tom/Downloads/thermo/20200721_GelatineStdsLaserJacobTest.csv",
-        use_analog=True,
+        "/home/tom/Desktop/20200721_GelatineStdsLaserJacobTest.csv", use_analog=True,
     )
     t2 = time.time()
     print(t1 - t0, t2 - t1)
