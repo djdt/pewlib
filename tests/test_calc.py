247f3ad56065ae09e604183493f4217787cbcc22
djdt <10266332+djdt@users.noreply.github.com>
2020-10-27 16:00:53 +1100

    update tests

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -10,24 +10,6 @@ def test_greyscale_to_rgb():
     )
 
 
-def test_kmeans():
-    x = np.empty((100, 2), dtype=float)
-    x[:50, 0] = np.random.normal(loc=-1.0, size=50)
-    x[50:, 0] = np.random.normal(loc=1.0, size=50)
-    x[:, 1] = np.random.normal(loc=3.0, size=100)
-
-    x = np.random.random(100)
-    y = np.zeros(100)
-    y[50:] += 1.0
-    y[80:] += 1.0
-
-    idx = calc.kmeans(np.stack((x, y), axis=1), 3, init="kmeans++")
-    _, counts = np.unique(idx, return_counts=True)
-    assert np.allclose(np.sort(counts), [20, 30, 50], atol=5)
-    idx = calc.kmeans(np.stack((x, y), axis=1), 3, init="random")
-    _, counts = np.unique(idx, return_counts=True)
-    assert np.allclose(np.sort(counts), [20, 30, 50], atol=10)
-
 
 def test_local_maxima():
     x = np.linspace(0.0, 1.0, 100)
