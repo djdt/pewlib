7efb061ba7688a101cb7ed844adf42183c2c3933
djdt <10266332+djdt@users.noreply.github.com>
2020-11-11 18:09:23 +1100

    working example

--- pew/io/__init__.py
+++ pew/io/__init__.py
@@ -1,6 +1,7 @@
 from . import agilent
 from . import error
 from . import npz
+from . import perkinelmer
 from . import textimage
 from . import thermo
 from . import vtk
--- pew/io/perkinelmer.py
+++ pew/io/perkinelmer.py
@@ -1,13 +1,9 @@
 import numpy as np
+import numpy.lib.recfunctions
 
 from pathlib import Path
 
-from typing import Any, Dict, List, Union
-
-
-# parameters_path = Path("parameters.conf")
-# positions_path = Path("positions.txt")
-# parameters_key_conv = {"ablation.speed": "speed", "sq"}
+from typing import List, Union
 
 
 def collect_datafiles(path: Path) -> List[Path]:
@@ -22,24 +18,11 @@ def collect_datafiles(path: Path) -> List[Path]:
     return datafiles
 
 
-# def xl_valid_lines(csv: str) -> Generator[bytes, None, None]:
-#     delimiter_count = 0
-#     past_header = False
-#     with open(csv, "rb") as fp:
-#         for line in fp:
-#             if past_header and line.count(b",") == delimiter_count:
-#                 yield line
-#             if line.startswith(b"Time"):
-#                 past_header = True
-#                 delimiter_count = line.count(b",")
-#                 yield line
-
-
-def load(path: Union[str, Path]) -> np.ndarray:
+def load(path: Union[str, Path], full: bool = False) -> np.ndarray:
     key_exchange = {
-        "speed": "ablation.speed",
-        "scantime": "acquisition.time",
-        "spotsize": "space.interval",
+        "ablation.speed": "speed",
+        "acquisition.time": "scantime",
+        "space.interval": "spotsize",
     }
     if not isinstance(path, Path):
         path = Path(path)
@@ -51,22 +34,26 @@ def load(path: Union[str, Path]) -> np.ndarray:
             for df in datafiles
         ]
     )
-    params: Dict[str, Any] = {"origin": (0.0, 0.0)}
+    data = numpy.lib.recfunctions.drop_fields(data, "Time_in_Seconds")
+    params: dict = {"origin": (0.0, 0.0)}
 
     parameters = path.joinpath("parameters.conf")
     if parameters.exists():
         with parameters.open() as fp:
-            params.update(
-                {k: v for k, v in (line.split("=") for line in fp.readlines())}
-            )
+            for line in fp:
+                if "=" in line:
+                    k, v = line.split("=")
+                    params[key_exchange[k.strip()]] = v.strip()
 
-        for new, old in key_exchange.items():
-            if old in params:
-                params[new] = params.pop(old)
+    # positions = path.joinpath("positions.txt")
+    # if positions.exists():
+    #     np.genfromtxt(
+    #         (line for line in positions.open() if "," in line),
+    #         delimiter=",",
+    #         dtype=float,
+    #     )
 
-    positions = path.joinpath("positions.txt")
-    if positions.exists():
-        np.genfromtxt(positions, delimiter=",", usecols=(0,1,2,3), dtype=float)
-
-
-load("/home/tom/Downloads/Coin")
+    if full:
+        return data, params
+    else:
+        return data
