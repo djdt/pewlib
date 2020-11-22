56dcd85f548ff5f68c86a507f9e259ce24bca618
djdt <10266332+djdt@users.noreply.github.com>
2020-11-22 12:38:36 +1100

    update setup

--- setup.py
+++ setup.py
@@ -4,11 +4,17 @@ from pew import __version__
 
 setup(
     name="pew",
-    description="Import / export library for LA-ICP-MS data.",
-    author="djdt",
+    description="Import, export and processing library for LA-ICP-MS data.",
+    url="https://github.com/djdt/pew",
+    author="T. Lockwood",
+    author_email="thomas.lockwood@uts.edu.au",
     version=__version__,
     packages=find_packages(include=["pew", "pew.*"]),
     install_requires=["numpy"],
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
+    project_urls={
+        "Documentation": "https://djdt.github.io/pew",
+        "Source": "https://gtihub.com/djdt/pew",
+    },
 )
