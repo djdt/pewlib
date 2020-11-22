ab4a34b528b7377ae1b09c0526a70780f06c93f6
djdt <10266332+djdt@users.noreply.github.com>
2020-11-22 13:11:49 +1100

    use readme as desc

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("README.md") as fp:
+    long_description = fp.read()
+
 setup(
     name="pewlib",
     description="Import, processing and export library for LA-ICP-MS data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/djdt/pewlib",
     author="T. Lockwood",
     author_email="thomas.lockwood@uts.edu.au",
