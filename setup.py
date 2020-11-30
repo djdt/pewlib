5bb463126ab096369931025057e53a97d9565b6d
djdt <10266332+djdt@users.noreply.github.com>
2020-12-01 10:19:18 +1100

    bump version

--- setup.py
+++ setup.py
@@ -5,19 +5,18 @@ with open("README.md") as fp:
 
 setup(
     name="pewlib",
+    version="0.6.3",
     description="Import, processing and export library for LA-ICP-MS data.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/djdt/pewlib",
     author="T. Lockwood",
     author_email="thomas.lockwood@uts.edu.au",
-    version="0.6.2",
-    packages=find_packages(include=["pewlib", "pewlib.*"]),
-    install_requires=["numpy"],
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest"],
+    url="https://github.com/djdt/pewlib",
     project_urls={
         "Documentation": "https://djdt.github.io/pewlib",
         "Source": "https://gtihub.com/djdt/pewlib",
     },
+    packages=find_packages(include=["pewlib", "pewlib.*"]),
+    install_requires=["numpy"],
+    tests_require=["pytest"],
 )
