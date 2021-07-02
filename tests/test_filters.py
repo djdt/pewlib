ad83762b666b7af7674c3994077be1fe1f672867
djdt <10266332+djdt@users.noreply.github.com>
2021-07-02 11:52:43 +1000

    add more sensitive filter tests

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -4,6 +4,12 @@ from pewlib.process import filters
 
 
 def test_mean_filter_1d():
+    # test specific, 1.6 filtered, 1.5 not
+    y = np.array([1.0, 1.1, 1.5, 1.3, 1.2, 1.1, 1.0, 1.6, 1.2, 1.3])
+    f = filters.rolling_mean(y, 5, threshold=3.0)
+    assert np.all(f == [1.0, 1.1, 1.5, 1.3, 1.2, 1.1, 1.0, 1.15, 1.2, 1.3])
+
+    # test random
     np.random.seed(93546376)
     y = np.sin(np.linspace(0, 10, 100))
     y[5::10] += np.random.choice([-1, 1], size=10)
@@ -29,6 +35,11 @@ def test_mean_filter_2d():
 
 
 def test_median_filter_1d():
+    # test specific, 1.6 filtered, 1.5 not
+    y = np.array([1.0, 1.1, 1.5, 1.3, 1.2, 1.1, 1.0, 1.6, 1.2, 1.3])
+    f = filters.rolling_median(y, 5, threshold=3.0)
+    assert np.all(f == [1.0, 1.1, 1.5, 1.3, 1.2, 1.1, 1.0, 1.2, 1.2, 1.3])
+
     np.random.seed(93546376)
     y = np.sin(np.linspace(0, 10, 100))
     y[5::10] += np.random.choice([-1, 1], size=10)
