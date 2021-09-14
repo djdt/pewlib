8141cd18fd79d886bd0a845e3b37711d2536dd92
djdt <10266332+djdt@users.noreply.github.com>
2021-09-14 15:04:18 +1000

    add testing and improvements to register module

--- pewlib/process/register.py
+++ pewlib/process/register.py
@@ -1,8 +1,10 @@
+"""Module for registering and merging images."""
+
 import numpy as np
 
 from pewlib.laser import Laser
 
-from typing import Union, Tuple
+from typing import Iterable, Union, Tuple
 
 
 def fft_register_images(a: np.ndarray, b: np.ndarray) -> Tuple[int, ...]:
@@ -28,36 +30,66 @@ def fft_register_images(a: np.ndarray, b: np.ndarray) -> Tuple[int, ...]:
     return np.unravel_index(np.argmax(xcorr), xcorr.shape) - np.array(xcorr.shape) // 2
 
 
-def overlap_structured_arrays(
-    a: np.ndarray, b: np.ndarray, anchor: Tuple[int, ...], fill: float = np.nan
+def overlap_arrays(
+    a: np.ndarray,
+    b: np.ndarray,
+    offset: Iterable[int],
+    fill: float = np.nan,
+    mode: str = "replace",
 ) -> np.ndarray:
-    """Merges two arrays by enlarging. Non-overlapping areas are filled with 'fill'.
+    """Merges two arrays by enlarging.
 
-    Coordinates (0, 0) of array 'b' will be at 'anchor' in the new array.
-    Shared names in 'a' are overwritten by 'b' where overlap occurs.
+    Coordinates (0, 0) of array `b` will be at `offset` in the new array.
+    Non-overlapping areas are filled with `fill`.
+    Overlapped values are calculated using `mode`. Mode 'replace' will use values in `b`,
+    'mean' and 'sum' uses the mean and sum of `a` and `b` respectively.
 
     Args:
         a: ndim array
         b: ndim array
-        anchor: offset for 'b', any int
-        fill: value to fill non-overlapping areas
+        offset: offset for 'b', any int
+        fill: value for non-overlapping areas
+        mode: value for overlapped areas, {'replace', 'mean', 'sum'}
 
     Returns:
-        new overlapped array
+        new overlapped array, same dtype as 'a'
+
+    Example
+    -------
+
+    >>> import numpy as np
+    >>> from pewlib.process import register
+    >>> a = np.arange(9.0).reshape(3, 3)
+    >>> b = np.arange(9.0).reshape(3, 3)
+    >>> c = register.overlap_arrays(a, b, fill=0, mode="sum")
+
+
+    .. plot::
+
+        import numpy as np
+        from pewlib.process import register
+        import matplotlib.pyplot as plt
+        c = register.overlap_arrays(np.arange(9.0).reshape(3, 3), np.arange(9.0).reshape(3, 3), fill=0, mode="sum")
+
+        plt.imshow(c)
+        plt.show()
     """
-    if a.ndim != b.ndim:
+
+    if a.ndim != b.ndim:  # pragma: no cover
         raise ValueError(
             f"Arrays 'a' and 'b' must have the same dimensions, {a.ndim} != {b.ndim}."
         )
 
-    offset = np.array(anchor, dtype=int)
-    if offset.size != a.ndim:
-        raise ValueError("Anchor must have same dimensions as 'a'.")
+    offset = np.array(offset, dtype=int)
+    if offset.size != a.ndim:  # pragma: no cover
+        raise ValueError("Offset must have same dimensions as arrays.")
+
+    if mode not in ["replace", "mean", "sum"]:  # pragma: no cover
+        raise ValueError("'mode' must be 'replace', 'mean' or 'sum'.")
 
     # find the max / min extents of the new array
     new_shape = np.maximum(a.shape, offset + b.shape) - np.minimum(0, offset)
-    new_dtype = list(set(a.dtype.descr + b.dtype.descr))
-    c = np.full(new_shape, fill, dtype=new_dtype)
+    c = np.full(new_shape, fill, dtype=a.dtype)
 
     # offsets for each array
     aoffset = np.where(offset < 0, -offset, 0)
@@ -65,55 +97,74 @@ def overlap_structured_arrays(
     aslice = tuple(slice(o, o + s) for o, s in zip(aoffset, a.shape))
     bslice = tuple(slice(o, o + s) for o, s in zip(boffset, b.shape))
 
-    for name in a.dtype.names:
-        c[name][aslice] = a[name]
-    for name in b.dtype.names:
-        c[name][bslice] = b[name]
+    # size and slice of overlapping area
+    abslice = tuple(slice(max(a.start, b.start), min(a.stop, b.stop), None) for a, b in zip(aslice, bslice))
+    absize = tuple(s.stop - s.start for s in abslice)
+    hasoverlap = all(x > 0 for x in absize)
+
+    # sections of overlap from a, b
+    asec = tuple(slice(o, o + s) for o, s in zip(aoffset, absize))
+    bsec = tuple(slice(o, o + s) for o, s in zip(boffset, absize))
+
+    c[aslice] = a
+    c[bslice] = b
+
+    if mode == "replace":
+        pass
+    elif mode == "mean" and hasoverlap:
+        c[abslice] = np.mean([a[asec], b[bsec]], axis=0)
+    elif mode == "sum" and hasoverlap:
+        c[abslice] = np.sum([a[asec], b[bsec]], axis=0)
+
     return c
 
 
-def overlap_lasers(
-    a: Laser, b: Laser, anchor: Union[str, Tuple[int, int]] = "top left"
-) -> Laser:
-    """Merges two arrays by enlarging.
+def overlap_structured_arrays(
+    a: np.ndarray,
+    b: np.ndarray,
+    offset: Iterable[int],
+    fill: float = np.nan,
+    mode: str = "replace",
+) -> np.ndarray:
+    """Merges two structured arrays by enlarging.
 
-    Coordinates (0, 0) of array 'b' will be at 'anchor' in the new array.
-    Shared names and calibrations in 'a' are overwritten by 'b' where overlap occurs.
-    Valid anchors are 'top left', 'top right', 'bottom left', 'bottom right', 'center' or
-    a tuple of ints sepcififying the coordinates of overlap.
+    Coordinates (0, 0) of array `b` will be at `offset` in the new array.
+    Shared names in 'a' and 'b' are calculated using `mode` where overlap occurs.
 
     Args:
-        a: Laser
-        b: Laser
-        anchor: offset
+        a: ndim array
+        b: ndim array
+        offset: offset for 'b'
+        fill: value for non-overlapping areas
+        mode: value for overlapped areas, {'replace', 'mean', 'sum'}
 
     Returns:
         new overlapped array
+
+    See Also:
+        :func:`pewlib.process.register.overlap_arrays`
     """
-    if isinstance(anchor, str):
-        if anchor == "top left":
-            anchor = (0, 0)
-        elif anchor == "top right":
-            anchor = (0, a.shape[1] - b.shape[1])
-        elif anchor == "bottom left":
-            anchor = (a.shape[0] - b.shape[0], 0)
-        elif anchor == "bottom right":
-            anchor = (a.shape[0] - b.shape[0], a.shape[1] - b.shape[1])
-        elif anchor == "center":
-            anchor = (np.array(a.shape, dtype=int) + b.shape) // 2 - b.shape
+    offset = np.array(offset, dtype=int)
+    if offset.size != a.ndim:  # pragma: no cover
+        raise ValueError("Anchor must have same dimensions as arrays.")
+
+    # find the max / min extents of the new array
+    new_shape = np.maximum(a.shape, offset + b.shape) - np.minimum(0, offset)
+    new_dtype = list(set(a.dtype.descr + b.dtype.descr))
+    c = np.empty(new_shape, dtype=new_dtype)
+
+    for name in c.dtype.names:
+        if name not in b.dtype.names:
+            c[name] = overlap_arrays(
+                np.full(b.shape, fill), a[name], offset=-offset, fill=fill, mode=mode
+            )
+        elif name not in a.dtype.names:
+            c[name] = overlap_arrays(
+                np.full(a.shape, fill), b[name], offset=offset, fill=fill, mode=mode
+            )
         else:
-            raise ValueError("Unknown anchor string.")
+            c[name] = overlap_arrays(
+                a[name], b[name], offset=offset, fill=fill, mode=mode
+            )
 
-    data = overlap_structured_arrays(a.data, b.data, anchor=anchor)
-
-    calibration = {}
-    for name in data.dtype.names:
-        if name in b.elements:
-            calibration[name] = b.calibration[name]
-        elif name in a.elements:
-            calibration[name] = a.calibration[name]
-
-    info = {"Overlap": f"{a.info['Name']}:{b.info['Name']}@{anchor[0],anchor[1]}"}
-    info.update(a.info)
-
-    return Laser(data=data, calibration=calibration, config=a.config, info=info)
+    return c
---
+++ tests/test_register.py
@@ -0,0 +1,91 @@
+import numpy as np
+
+from pewlib.process import register
+
+
+def test_fft_regsiter_images():
+    np.random.seed(940926)
+
+    # random no offset
+    a = np.random.random((100, 100))
+    b = np.random.random((100, 100))
+
+    offset = register.fft_register_images(a, b)
+    assert np.all(offset == (0, 0))
+
+    # perfect overlap circles
+    xx, yy = np.mgrid[:100, :100]
+    a[(xx - 50) ** 2 + (yy - 75) ** 2 < 50] += 5.0
+    b[(xx - 25) ** 2 + (yy - 25) ** 2 < 50] += 5.0
+
+    offset = register.fft_register_images(a, b)
+    assert np.all(offset == (25, 50))
+    offset = register.fft_register_images(b, a)
+    assert np.all(offset == (-25, -50))
+
+    # bars
+    a = np.random.random((100, 100))
+    b = np.random.random((100, 100))
+
+    a[10:20] += 5.0
+    b[60:70] += 5.0
+
+    offset = register.fft_register_images(a, b)
+    assert np.all(offset == (-50, 0))
+    offset = register.fft_register_images(b, a)
+    assert np.all(offset == (50, 0))
+
+
+def test_overlap_arrays():
+    a = np.arange(9.0).reshape(3, 3)
+    b = np.arange(9.0).reshape(3, 3)
+    import matplotlib.pyplot as plt
+
+    # Test overlap methods
+    c = register.overlap_arrays(a, b, offset=(1, 1), mode="replace")
+    assert c.shape == (4, 4)
+    assert np.isnan(c[0, 3])
+    assert np.isnan(c[3, 0])
+    assert np.all(c[~np.isnan(c)] == [0, 1, 2, 3, 0, 1, 2, 6, 3, 4, 5, 6, 7, 8])
+    c = register.overlap_arrays(a, b, offset=(1, 1), mode="mean")
+    assert np.all(c[~np.isnan(c)] == [0, 1, 2, 3, 2, 3, 2, 6, 5, 6, 5, 6, 7, 8])
+    c = register.overlap_arrays(a, b, offset=(1, 1), mode="sum")
+    assert np.all(c[~np.isnan(c)] == [0, 1, 2, 3, 4, 6, 2, 6, 10, 12, 5, 6, 7, 8])
+
+    # Test various offsets
+    for i in range(-5, 5):
+        for j in range(-5, 5):
+            c = register.overlap_arrays(a, b, offset=(i, j), mode="sum")
+            assert np.nansum(c) == 72.0
+
+    # Test non nan fill
+    c = register.overlap_arrays(a, b, offset=(-3, 0), fill=0.0, mode="replace")
+    assert np.all(c[:3] == b)
+    c = register.overlap_arrays(a, b, offset=(-3, 0), fill=0.0, mode="mean")
+    assert np.all(c[:3] == b)
+    c = register.overlap_arrays(a, b, offset=(-3, 0), fill=0.0, mode="sum")
+    assert np.all(c[:3] == b)
+
+
+
+def test_overlap_structured_arrays():
+    a = np.empty((4, 4), dtype=[("a", float), ("c", float)])
+    a["a"].flat = np.arange(16.0)
+    a["c"].flat = np.arange(16.0)
+    b = np.empty((4, 4), dtype=[("b", float), ("c", float)])
+    b["b"].flat = np.arange(16.0)
+    b["c"] = 1.0
+
+    d = register.overlap_structured_arrays(a, b, offset=(2, 2), mode="replace")
+
+    # Test overlap methods
+    assert d.shape == (6, 6)
+    assert np.all(d["a"][:4, :4] == a["a"])
+    assert np.all(d["b"][2:, 2:] == b["b"])
+    assert np.all(d["c"][2:, 2:] == 1.0)
+    assert np.all(d["c"][:2, :2] == a["a"][:2, :2])
+
+    d = register.overlap_structured_arrays(a, b, offset=(2, 2), mode="mean")
+
+test_overlap_arrays()
+test_overlap_structured_arrays()
