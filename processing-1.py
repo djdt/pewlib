e1be43b87732c28d282238fc0328cdc1b8a999d0
djdt <10266332+djdt@users.noreply.github.com>
2021-09-14 05:08:06 +0000

    Deploying to docs from  @ f15a021f22d059f92f413a9a199a55c66ddf8da8 🚀

--- processing-1.py
+++ processing-1.py
@@ -1,8 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from pewlib.process import filters
-import numpy as np
-from pewlib.process import filters
 a = np.sin(np.linspace(0, 10, 50))
 a[5::10] +=np.random.choice([-1, 1], size=5)
 b = filters.rolling_mean(a, 3, threshold=3.0)
