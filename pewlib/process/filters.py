cc7b20d056da19b1aeae0d333e74a423db06956e
djdt <10266332+djdt@users.noreply.github.com>
2022-05-22 15:56:54 +1000

    Remove nan based functions from rolling filters, increases speed.

--- pewlib/process/filters.py
+++ pewlib/process/filters.py
@@ -53,23 +53,23 @@ def rolling_mean(
         plt.show()
     """
     if isinstance(block, int):
-        block = tuple([block])
+        block = tuple([block] * x.ndim)
     assert len(block) == x.ndim
 
     # Prepare array by padding with nan
     pads = [(b // 2, b // 2) for b in block]
     x_pad = np.pad(x, pads, constant_values=np.nan)
 
-    blocks = view_as_blocks(x_pad, block, tuple([1 for b in block]))
+    blocks = view_as_blocks(x_pad, block, tuple([1] * x.ndim))
 
     # Calculate means and stds
     axes = tuple(np.arange(x.ndim, x.ndim * 2))
-    means = np.nanmean(blocks, axis=axes)
+    means = np.mean(blocks, axis=axes)
 
     # Don't include the central point in the std calculation
-    nancenter = np.ones(block, dtype=np.float64)
-    nancenter[np.array(nancenter.shape) // 2] = np.nan
-    stds = np.nanstd(blocks * nancenter, axis=axes)
+    # nancenter = np.ones(block, dtype=np.float64)
+    # nancenter[np.array(nancenter.shape) // 2] = np.nan
+    stds = np.std(blocks, axis=axes)
 
     # Check for outlying values and set as nan
     outliers = np.abs(x - means) > threshold * stds
@@ -77,14 +77,16 @@ def rolling_mean(
     unpads = tuple([slice(p[0], -p[1]) for p in pads])
     x_pad[unpads][outliers] = np.nan
 
-    # As the mean is sensitive to outliers reclaculate it
+    # As the mean is sensitive to outliers recalculate it
     means = np.nanmean(blocks, axis=axes)
 
     return np.where(np.logical_and(outliers, ~np.isnan(means)), means, x)
 
 
 def rolling_median(
-    x: np.ndarray, block: Union[int, Tuple[int, ...]], threshold: float = 3.0
+    x: np.ndarray,
+    block: Union[int, Tuple[int, ...]],
+    threshold: float = 3.0,
 ) -> np.ndarray:
     """Filter an array using rolling median.
 
@@ -96,7 +98,7 @@ def rolling_median(
     Args:
         x: array
         block: size of window, int or same dims as `x`
-        threshold: number of median distances away from medians to consider outlier
+        threshold: number of SDs (via MAD) away from median to consider outlier
 
     Returns:
         array with outliers set to local means
@@ -130,27 +132,27 @@ def rolling_median(
 
     """
     if isinstance(block, int):  # pragma: no cover
-        block = tuple([block])
+        block = tuple([block] * x.ndim)
     assert len(block) == x.ndim
 
     # Prepare array by padding with nan
     pads = [(b // 2, b // 2) for b in block]
-    x_pad = np.pad(x, pads, constant_values=np.nan)
+    y = np.pad(x, pads, constant_values=np.nan)
 
-    blocks = view_as_blocks(x_pad, block, tuple([1 for b in block]))
+    blocks = view_as_blocks(y, block, tuple([1] * x.ndim))
 
     # Calculate median and differences
     axes = tuple(np.arange(x.ndim, x.ndim * 2))
-    medians = np.nanmedian(blocks, axis=axes)
+    medians = np.median(blocks, axis=axes)
 
     # Remove padding
     unpads = tuple([slice(p[0], -p[1]) for p in pads])
-    x_pad[unpads] = np.abs(x - medians)
+    y[unpads] = np.abs(x - medians)
 
     # Median of differences
-    mad = np.nanmedian(blocks, axis=axes) * 1.4826  # estimate stddev
+    mad = np.median(blocks, axis=axes) * 1.4826  # estimate stddev
 
     # Outliers are n medians from data
-    outliers = x_pad[unpads] > threshold * mad
+    outliers = y[unpads] > threshold * mad
 
     return np.where(np.logical_and(outliers, ~np.isnan(medians)), medians, x)
