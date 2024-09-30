4304dc8ae3e5756b25f85d928b8d52305724c918
djdt <10266332+djdt@users.noreply.github.com>
2024-09-30 16:58:41 +1000

    fix test for tic

--- tests/test_io_imzml.py
+++ tests/test_io_imzml.py
@@ -28,17 +28,25 @@ def test_io_imzml():
     assert imz.intensity_params.dtype == np.float32
 
     tic = imz.extract_tic()
-
     assert tic.shape == (2, 2)
-    assert np.allclose(tic, [[72098.0, 80436.0], [75608.0, 52676.0]])
+    print(tic)
+    assert np.allclose(tic, [[52676.0,  75608.0], [80436.0, 72098.0]])
 
     targets = imz.extract_masses([300.0, 600.0, 900.0])
     assert targets.shape == (2, 2, 3)
 
-    mz00 = imz.spectra[(0, 0)].get_binary_data(
+    mz00 = imz.spectra[(1, 1)].get_binary_data(
         imz.mz_params.id, imz.mz_params.dtype, imz.external_binary
     )
-
-    assert mz00.shape == (852, )
+    assert mz00.shape == (852,)
     assert np.isclose(np.amin(mz00), 200.273186)
     assert np.isclose(np.amax(mz00), 992.453628)
+
+    mz_range = imz.mass_range()
+    assert np.allclose(mz_range, (200.081016, 992.925155))
+
+    bins, data = imz.binned_masses(mass_width_mz=1.0)
+    assert bins.size == int((mz_range[1] + 1.0) - mz_range[0]) + 1
+
+    assert data.shape == (2, 2, 794)
+    assert data[1][1][0] == 16.0
