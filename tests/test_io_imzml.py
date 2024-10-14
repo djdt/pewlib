587852b9b83fe1105578d9ce401b3960e9fd8faa
djdt <10266332+djdt@users.noreply.github.com>
2024-10-14 16:32:22 +1100

    test untargetted

--- tests/test_io_imzml.py
+++ tests/test_io_imzml.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import numpy as np
+import pytest
 
 from pewlib.io import imzml
 
@@ -16,7 +17,8 @@ def test_is_imzml():
 def test_io_imzml():
     path = Path(__file__).parent.joinpath("data", "imzml")
 
-    imz = imzml.ImzML.from_file(path.joinpath("test.imzML"), path.joinpath("test.ibd"))
+    # auto find .ibd
+    imz = imzml.ImzML.from_file(path.joinpath("test.imzML"))
 
     assert imz.scan_settings.image_size == (2, 2)
     assert imz.scan_settings.pixel_size == (30.0, 30.0)
@@ -29,11 +31,16 @@ def test_io_imzml():
 
     tic = imz.extract_tic()
     assert tic.shape == (2, 2)
-    print(tic)
-    assert np.allclose(tic, [[52676.0,  75608.0], [80436.0, 72098.0]])
+    assert np.allclose(tic, [[52676.0, 75608.0], [80436.0, 72098.0]])
 
-    targets = imz.extract_masses([300.0, 600.0, 900.0])
+    targets = imz.extract_masses([300.0, 600.0, 900.0], mass_width_ppm=10.0)
     assert targets.shape == (2, 2, 3)
+
+    targets = imz.extract_masses([300.0, 600.0, 900.0], mass_width_mz=1.0)
+    assert targets.shape == (2, 2, 3)
+
+    with pytest.raises(ValueError):
+        imz.extract_masses([500.0], mass_width_mz=1.0, mass_width_ppm=1.0)
 
     mz00 = imz.spectra[(1, 1)].get_binary_data(
         imz.mz_params.id, imz.mz_params.dtype, imz.external_binary
@@ -50,3 +57,10 @@ def test_io_imzml():
 
     assert data.shape == (2, 2, 794)
     assert data[1][1][0] == 16.0
+
+    mzs, signals = imz.untargeted_extraction(
+        100, min_pixel_count=3, min_height_absolute=10.0
+    )
+    assert signals.shape[:2] == (2, 2)
+    assert len(mzs) == signals.shape[-1]
+
